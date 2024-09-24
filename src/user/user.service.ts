07aac01f89a217e78ec27a02ce6a6b369d1fb710
你的笑很美 <39164882+smileBeauty@users.noreply.github.com>
2024-09-25 01:31:58 +0800

    Create user.service.ts

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ id });
+  }
+}
