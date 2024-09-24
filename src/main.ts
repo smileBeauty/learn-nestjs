7e75ba9ab95cb5643088ed23b0c01b9bff2b9311
你的笑很美 <39164882+smileBeauty@users.noreply.github.com>
2024-09-25 01:29:16 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   const address = await app.getUrl();
   const logger = new Logger(bootstrap.name, { timestamp: true });
   logger.log(`server on ${address}`);
