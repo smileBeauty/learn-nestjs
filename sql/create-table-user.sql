b4c15893e887c33f948de3abdc2b40ab68edb4b5
你的笑很美 <39164882+smileBeauty@users.noreply.github.com>
2024-09-26 18:23:22 +0800

    Update create-table-user.sql

--- sql/create-table-user.sql
+++ sql/create-table-user.sql
@@ -1,11 +1,10 @@
-CREATE TABLE `local`.`t_user` (
-  `id` VARCHAR(255) NOT NULL COMMENT 'id',
-  `username` VARCHAR(255) NULL COMMENT '用户名',
-  `password` VARCHAR(255) NULL COMMENT '密码',
-  `telephone` VARCHAR(45) NULL COMMENT '手机号',
-  `birthday` DATE NULL COMMENT '生日',
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci
-COMMENT = '用户表';
+CREATE TABLE `t_user` (
+  `id` varchar(255) NOT NULL COMMENT 'id',
+  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `telephone` varchar(45) DEFAULT NULL COMMENT '手机号',
+  `birthday` date DEFAULT NULL COMMENT '生日',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表'
