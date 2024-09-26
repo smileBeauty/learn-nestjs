7a37efe48d9d72de250bb44bccdd51d1c9c3ae9a
你的笑很美 <39164882+smileBeauty@users.noreply.github.com>
2024-09-26 18:24:26 +0800

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/')
+  @Post('/')
   async findAllUser() {
     return await this.userService.findAll();
+  }
+
+  @Post('/page')
+  async findUserPage() {
+    return await this.userService.findUserPage();
   }
 }
